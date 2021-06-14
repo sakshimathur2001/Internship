b20f7a675ce3045a1c01013fde298c46b4b70847
sakshimathur2001 <mathursakshi2001@gmail.com>
2021-06-14 22:48:31 +0530

    updated the JSON file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=express();
 app.set('view-engine', 'ejs')
 
-app.get("/", function(req,responce){
+app.get('/', function(req,responce){
     responce.render('home.ejs',{data:'Hello World!'});
   });
 /*app.listen(3000,function(){
