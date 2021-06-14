284f188e909bed4cfd9b96c0adfa0b54966a7a61
sakshimathur2001 <mathursakshi2001@gmail.com>
2021-06-14 20:22:34 +0530

    changed index file

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.set('view-engine', 'ejs')
 app.get("/", function(req,responce){
     responce.render('home.ejs',{data:'Hello World!'});
   });
-app.listen(3000,function(){
+/*app.listen(3000,function(){
     console.log("sever started at port 3000");
+  });*/
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
